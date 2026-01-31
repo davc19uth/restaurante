b862331ba899f9f69f7f7ccf597b8ef3edec5c76
davc19uth <davc19uth@gmail.com>
2026-01-31 17:50:34 -0600

    Add files via upload

--- menuRestaurante.py
+++ menuRestaurante.py
@@ -1,9 +1,34 @@
 import json
+import os
+import sys
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.resources import resource_add_path
+from kivy.lang import Builder
+
+# ---------- Rutas Imagenes ----------
+def ruta_asset(ruta_relativa):
+    try:
+        base_path = sys._MEIPASS
+    except:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, ruta_relativa)
+
+
+# ---------- Ruta JSON ----------
+def ruta_json():
+    if getattr(sys, 'frozen', False):
+        ruta_base = sys._MEIPASS
+    else:
+        ruta_base = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(ruta_base, 'menuPrincipal.json')
+
+# Agregar ruta de assets
+resource_add_path(ruta_asset("."))
+resource_add_path(ruta_asset("assets"))
 
 # ---------- Pantallas ----------
 class InicioScreen(Screen):
@@ -35,8 +60,11 @@ class MenuApp(App):
     def build(self):
         self.title = "Menú del Restaurante"
 
+        # ⬇️ Cargar KV manualmente
+        Builder.load_file(ruta_asset("menu.kv"))
+        
         # Cargar JSON
-        with open("menuPrincipal.json", encoding="utf-8") as f:
+        with open(ruta_json(), encoding="utf-8") as f:
             data = json.load(f)
             self.productos = data["productos"]
 
---
+++ readme.txt
@@ -0,0 +1,13 @@
+La aplicacón menuRestaurante proporciona al usuario un menu de 3 opciones:
+    - Comidas
+    - Bebidas
+    - Postres
+
+En la pantalla inicial esta la opción de ver el menu, este dirige a una segunda pantalla
+la cual muestra las 3 opciones enumeradas anteriormente.
+
+Al ingresar a cada una de las opciones se listan los productos que ofrece el menu junto
+con el precio del mismo, al final de cada pantalla se encuentra la opción para regresar
+a la pantalla anterior.
+
+La aplicación se encapsla en una archivo ejecutable, el cual se ejecuta al abrir el mismo.
